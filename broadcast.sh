0fcf4b7523f3f2f411d657390fb64ca38ec48de9
Maya Doshi <maya-doshi@proton.me>
2025-12-18 15:11:23 -0500

    broadcast: reduce sample rate on the SDR to improve stability

--- broadcast.sh
+++ broadcast.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
-rtl_fm -g 50 -f 88.3M -M wfm -s 180k -E deemp | sox -t raw -r 180k -e signed -b 16 -c 1 -V1 - -t mp3 - remix 1 lowpass 16k | ezstream -c /home/wrct/stream/configs/ezstream-broadcast.xml
+rtl_fm -f 88.3M -M wfm -s 160k -E deemp -F 9 \
+| sox -t raw -r 160k -e signed -b 16 -c 1 -V1 - -t mp3 -r 48000 -c 2 - remix 1 lowpass 16k \
+| ezstream -c /home/wrct/stream/configs/ezstream-broadcast.xml
+
 
 exit 1
