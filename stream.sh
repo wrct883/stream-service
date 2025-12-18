1bce2f398612ea510da44ea055f58e767826fb8c
Maya Doshi <maya-doshi@proton.me>
2025-12-18 15:09:13 -0500

    stream: the axia channel changed and hence so did the magic number

--- stream.sh
+++ stream.sh
@@ -2,6 +2,6 @@
 
 set -eo pipefail
 
-rtpdump -F payload 0xefc01b59/5004 | sox -q -c 2 -r 48000 -b 24 -e signed-integer -B -t raw - -t mp3 - gain 5 | ezstream -c /home/wrct/stream/configs/ezstream-stdin.xml
+rtpdump -F payload 0xefc007da/5004 | sox -V2 -c 2 -r 48000 -b 24 -e signed-integer -B -t raw - -t mp3 -C 128 - gain 5 | ezstream -v -c /home/wrct/stream/configs/ezstream-stdin.xml
 
 exit 1
